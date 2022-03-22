e8ef18aea2d269c8f7d1963b962bf63ee196e93d
mihkelvaher <mihkel.vaher@industry62.com>
2022-03-22 15:15:46 +0200

    React algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>REACT</div>
+  );
+}
+
+export default App;
---
+++ veebipood/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// kommentaar
+// ctrl + ä kui aktiveerin koodi
+
+// README - tutvustus Reacti osas
+// package.json - koodilõikude kogumik (üldvaade)
+
+// React versioon 15 -> 17
+// package.json failis muudan ära 15 asemel 17
+// ja kirjutan cmd-sse "npm install"
+
+// package.json ---> omab infot node_modules
+//         sisu kohta
+
+// package-lock.json -> detailvaade
+//  seda ei muudeta, võin muuta, aga kasu ei oma
+
+// .gitignore -> mida ma Githubi üles ei pane
+// node_modules ei lähe, sest liiga suur
+//    lihtsasti genereeritav
+
+// node_module -> kõik erinevad koodilõigud, 
+//mida React vajab
+
+// public -> pildid, andmete failid
+
+// src -> kood - .js ja .css
