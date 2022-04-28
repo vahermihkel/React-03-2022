06f91d607566d1de5e45ebe98b09758b4ceabb2f
mihkelvaher <mihkel.vaher@industry62.com>
2022-04-28 16:13:52 +0300

    Toodete otsimine, Pakiautomaadid

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import cartStyles from "./css/Cart.module.css";
 
 function Cart() {
@@ -14,24 +14,26 @@ function Cart() {
   }
 
   function decreaseQuantity(clickedProduct) {
-    const index = cartProducts.findIndex(
-      element => element.product.id === clickedProduct.product.id);
-    cartProducts[index].quantity--;
-    if (cartProducts[index].quantity === 0) {
-      removeFromCart(clickedProduct);
+    if (clickedProduct.product.id !== "11122333") {
+      const index = cartProducts.findIndex(
+        element => element.product.id === clickedProduct.product.id);
+      cartProducts[index].quantity--;
+      if (cartProducts[index].quantity === 0) {
+        removeFromCart(clickedProduct);
+      }
+      setCartProducts(cartProducts.slice());
+      sessionStorage.setItem("products", JSON.stringify(cartProducts));
     }
-    setCartProducts(cartProducts.slice());
-    sessionStorage.setItem("products", JSON.stringify(cartProducts));
   }
 
   function increaseQuantity(clickedProduct) {
-    const index = cartProducts.findIndex(
-      element => element.product.id === clickedProduct.product.id);
-    cartProducts[index].quantity++;
-    setCartProducts(cartProducts.slice());
-    sessionStorage.setItem("products", JSON.stringify(cartProducts));
-    // 1. uuendama HTMLi
-    // 2. uuendama sessionStorage-t
+    if (clickedProduct.product.id !== "11122333") {
+      const index = cartProducts.findIndex(
+        element => element.product.id === clickedProduct.product.id);
+      cartProducts[index].quantity++;
+      setCartProducts(cartProducts.slice());
+      sessionStorage.setItem("products", JSON.stringify(cartProducts));
+    }
   }
 
   function removeFromCart(clickedProduct) {
@@ -40,6 +42,44 @@ function Cart() {
     cartProducts.splice(index,1);
     setCartProducts(cartProducts.slice());
     sessionStorage.setItem("products", JSON.stringify(cartProducts));
+    if (clickedProduct.product.id === "11122333") {
+      deleteSelectedOmniva();
+    }
+  }
+
+  const omnivaUrl = "https://www.omniva.ee/locations.json";
+  const [omnivaMachines, setOmnivaMachines] = useState([]); // List dropdowni sees
+  const [selectedOmniva, setSelectedOmniva] = useState(
+    sessionStorage.getItem("selectedOmnivaMachine")); // milline valiti
+  const omnivaRef = useRef(); // dropdowni sisse väärtust kuulav
+
+  useEffect(() => {
+    fetch(omnivaUrl).then(response => response.json())
+      .then(responseBody => setOmnivaMachines(responseBody))
+  },[]);
+
+  function selectOmnivaMachine() {
+    //console.log(omnivaRef.current.value);
+    const omnivaValue = omnivaRef.current.value;
+    const cartOmniva = {
+      product:
+      {
+        id: "11122333",
+        name: "Omniva pakiautomaadi tasu",
+        price: 3.5,
+        imgSrc: "/locker.png"
+      }, 
+      quantity: 1
+    }
+    cartProducts.push(cartOmniva);
+    sessionStorage.setItem("products", JSON.stringify(cartProducts));
+    sessionStorage.setItem("selectedOmnivaMachine", JSON.stringify(omnivaValue));
+    setSelectedOmniva(omnivaValue);
+  }
+
+  function deleteSelectedOmniva() {
+    setSelectedOmniva(null);
+    sessionStorage.removeItem("selectedOmnivaMachine"); 
   }
 
   return (
@@ -50,15 +90,38 @@ function Cart() {
         <div className={cartStyles.cartProductName}>{element.product.name}</div>
         <div className={cartStyles.cartProductPrice}>{Number(element.product.price).toFixed(2)} €</div>
         <div className={cartStyles.cartProductQuantityControls}>
-          <img className={cartStyles.cartProductButton} onClick={() => decreaseQuantity(element)} src="/cart/minus.png" alt="" />
-          <div className={cartStyles.cartProductQuantity}>{element.quantity}</div>
-          <img className={cartStyles.cartProductButton} onClick={() => increaseQuantity(element)} src="/cart/plus.png" alt="" />
+          { element.product.id !== "11122333" && <img 
+                      className={element.product.id === "11122333" ? 
+                        cartStyles.cartProductButtonDisabled : cartStyles.cartProductButton
+                      } 
+                      onClick={() => decreaseQuantity(element)} 
+                      src="/cart/minus.png" 
+                      alt="" />}
+            <div className={cartStyles.cartProductQuantity}>{element.quantity} tk</div>
+          { element.product.id !== "11122333" && <img 
+                      className={element.product.id === "11122333" ? 
+                          cartStyles.cartProductButtonDisabled : cartStyles.cartProductButton} 
+                      onClick={() => increaseQuantity(element)} 
+                      src="/cart/plus.png" 
+                      alt="" />}        
         </div>
         <div className={cartStyles.cartProductSum}>{(element.product.price * element.quantity).toFixed(2)} €</div>
         <img className={cartStyles.cartProductButton} onClick={() => removeFromCart(element)} src="/cart/delete.png" alt="" />
       </div>) 
     }
-    <br />  <br />
+    <br /> 
+
+    { !selectedOmniva && 
+      <select ref={omnivaRef} onChange={() => selectOmnivaMachine()}>
+        { omnivaMachines.map(element => <option value={element.NAME}>{element.NAME}</option>) }
+      </select>}
+    { selectedOmniva && 
+      <div>
+      {selectedOmniva} <button onClick={() => removeFromCart({product: {id: "11122333"}})}>X</button> 
+      </div>}
+
+    <br />
+    
   </div>)
 }
 
@@ -73,6 +136,8 @@ export default Cart;
 /* KODUS:
  Kustutamine
  Üksik toode
+ Dropdown - kategooriad editis
+ Keypress  .findIndex / .filter
 
  MEIE:
  TÄNA:
--- webshop/src/pages/admin/ViewProducts.jsx
+++ webshop/src/pages/admin/ViewProducts.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 function ViewProducts() {
   const [products, setProducts] = useState([]);
   const url = "https://react-03-22-default-rtdb.europe-west1.firebasedatabase.app/products.json";
+  const searchRef = useRef();
+  const [originalProducts, setOriginalProducts] = useState([]);
 
   useEffect(() => {
     fetch(url).then(response => response.json())
@@ -13,11 +15,34 @@ function ViewProducts() {
         productsFromDb.push(responseBody[key]);
       }
       setProducts(productsFromDb);
+      setOriginalProducts(productsFromDb);
     })
   },[]);
 
+  // Number("mingisugune-string")  <-- error, sest ei oska numbriks teha
+  // Number("12312312");   <--- ok
+  // 12312312.indexOf()   <-- error, kuna numbril ei ole indekseid
+  // "12312312".indexOf() <-- ok
+  // 12313123.toString()   <-- alati ok
+
+  function searchProducts() {
+    console.log(searchRef.current.value);
+                            //   .indexOf()
+    // searchRef  - mida sisestasin
+    // product.name.indexOf(searchRef)
+    // KUI LEIAB, siis on index >= 0
+    // KUI EI LEIA, siis on index -1
+    // filterdan sisse kõik kellel on index suurem kui 0
+    const searchedProduct = searchRef.current.value.toLowerCase();
+    const productsFound = originalProducts.filter(element => 
+      element.name.toLowerCase().indexOf(searchedProduct) >= 0 || 
+        element.id.toString().indexOf(searchedProduct) >= 0 );
+    setProducts(productsFound);
+  }
+
   return (
   <div>
+    <input ref={searchRef} onChange={() => searchProducts()} type="text" />
     {products.map(element => 
       <div>
         <img src={element.imgSrc} alt="" />
